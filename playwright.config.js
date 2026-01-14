8a69008555ed83fca5d2cc50a331a8f7eefb164d
MohammadMunim <aamunim@hotmail.com>
2026-01-14 00:05:25 -0400

    reset the retries option back to 1 and remove the trace section as duplicate

--- playwright.config.js
+++ playwright.config.js
@@ -30,11 +30,8 @@ testDir: './tests',
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  //retries: 1,
-  use: {
-    trace: 'on-first-retry', // record traces on first retry of each test
-  },
+  //retries: process.env.CI ? 2 : 0,
+  retries: 1,
 
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 1 : undefined,
