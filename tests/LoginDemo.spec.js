1dcfbbf48b9b75dfa47114337a2dca959b54e2b0
MohammadMunim <aamunim@hotmail.com>
2026-01-14 12:05:10 -0400

    fixed login demo

--- tests/LoginDemo.spec.js
+++ tests/LoginDemo.spec.js
@@ -1,9 +1,10 @@
 import{test, expect} from '@playwright/test';
 
+
 test('Demo login test web1', async ({page})=>{
 
 await page.goto('https://demo.applitools.com/');
-await page.pause();   
+//await page.pause() 
 await page.locator('[placeholder="Enter your username"]').fill('Raghav');
 await page.locator('[placeholder="Enter your password"]').fill('1234');
 await page.locator('text=Sign in').click();
@@ -19,13 +20,14 @@ test('Login demo test 2', async ({ page }) => {
    await page.locator('[placeholder="Username"]').fill('Admin')
    await page.locator('[placeholder="Password"]').fill('admin123')
    await page.locator('button:has-text("Login")').click()
-   await page.locator('p:has-text("Paul Collings")').click()
+   await page.locator('p:has-text("firstName lastName")').click()
    await page.locator('text=Logout').click()
    await page.close();
 })
-
+/*
 test.only('Login demo test 3', async ({ page }) => {
    await page.goto('https://admin-demo.nopcommerce.com/login?returnUrl=%2Fadmin%2F')
+   await page.pause()
    await page.locator('input[name="Email"]').click();
    await page.locator('input[name="Email"]').press('Control+a');
    await page.locator('input[name="Email"]').fill('admin@yourstore.com');
@@ -38,3 +40,4 @@ test.only('Login demo test 3', async ({ page }) => {
    await page.waitForURL('https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F%27%29%3B');
    await page.close();
 })
+*/
