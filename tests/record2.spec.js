abfda55e568494ccab575f0362bcf22f65d182f8
MohammadMunim <aamunim@hotmail.com>
2026-01-13 22:53:08 -0400

    first commit

---
+++ tests/record2.spec.js
@@ -0,0 +1,24 @@
+import {test, expect} from '@playwright/test';
+const { webkit, devices } = require('playwright');
+
+test ('record demo 2', async () => {
+  const browser = await webkit.launch({
+    headless: false
+  });
+  const context = await browser.newContext({
+    ...devices['iPhone 15 Pro Max'],
+  });
+  const page = await context.newPage();
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').press('Tab');
+  await page.locator('[data-test="password123"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+});
