8e5254ee9c7ec111885eeee6dce55a23596560ac
MohammadMunim <aamunim@hotmail.com>
2026-01-14 12:14:48 -0400

    Updated Login

--- tests/LoginDemo.spec.js
+++ tests/LoginDemo.spec.js
@@ -20,7 +20,7 @@ test('Login demo test 2', async ({ page }) => {
    await page.locator('[placeholder="Username"]').fill('Admin')
    await page.locator('[placeholder="Password"]').fill('admin123')
    await page.locator('button:has-text("Login")').click()
-   await page.locator('p:has-text("firstName lastName")').click()
+   await page.locator('p:has-text("Raju G")').click()
    await page.locator('text=Logout').click()
    await page.close();
 })
